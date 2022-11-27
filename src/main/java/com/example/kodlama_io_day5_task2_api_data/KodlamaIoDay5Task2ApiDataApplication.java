248df725e4d6d45a53b1bad5c44b9891c76a788a
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-28 00:07:31 +0100

    spring data- Api-Postman

---
+++ src/main/java/com/example/kodlama_io_day5_task2_api_data/KodlamaIoDay5Task2ApiDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodlama_io_day5_task2_api_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodlamaIoDay5Task2ApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodlamaIoDay5Task2ApiDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kodlama_io_day5_task2_api_data/KodlamaIoDay5Task2ApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodlama_io_day5_task2_api_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodlamaIoDay5Task2ApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
